087ea2ff15043a7857f4f07051f83b62f7b97bcf
dima666Sik <leniviy.demon.1@gmail.com>
2025-08-09 16:49:39 +0300

    skeleton of client and server sides

---
+++ server/src/main/java/ua/platform/coding/learning/LearningCodingApplication.java
@@ -0,0 +1,11 @@
+package ua.platform.coding.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningCodingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LearningCodingApplication.class, args);
+    }
+}
