8828d85ddc0e48f193bc3b83adae6d1c2b07f154
dima666Sik <leniviy.demon.1@gmail.com>
2025-08-09 20:25:21 +0300

    del unnecessary courses from init state in slice.

--- client/src/redux/slices/coursesSlice.js
+++ client/src/redux/slices/coursesSlice.js
@@ -4,10 +4,6 @@ const initialState = {
 	courses: [
 		{ id: 1, title: "Java Basics", description: "Learn Java fundamentals" },
 		{ id: 2, title: "React for Beginners", description: "Intro to React.js" },
-		{ id: 2, title: "React for Beginners", description: "Intro to React.js" },
-		{ id: 2, title: "React for Beginners", description: "Intro to React.js" },
-		{ id: 2, title: "React for Beginners", description: "Intro to React.js" },
-		{ id: 2, title: "React for Beginners", description: "Intro to React.js" },
 	],
 };
 
